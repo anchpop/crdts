194a5d60425457fc7f9af02b18dedb6ce655b6ac
Andre Popovitch <andre@popovit.ch>
2020-04-17 01:42:02 -0400

    automatic test for my test Nat CRDT

--- crdts/src/main.rs
+++ crdts/src/main.rs
@@ -1,74 +1,19 @@
-use iced::{button, Application, Button, Column, Command, Element, Settings, Text};
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
 
 mod replicant;
 use replicant::Semilattice;
 
 
-#[derive(Default)]
-struct Counter {
-    // The counter value
-    value: i32,
-
-    // The local state of the two buttons
-    increment_button: button::State,
-    decrement_button: button::State,
-}
-
-
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-    IncrementPressed,
-    DecrementPressed,
-}
-
-
-impl Application for Counter {
-    type Message = Message;
-    
-    fn new() -> (Self, Command<Message>) {
-        (Self::default(), Command::none())
-    }
-
-    
-    fn title(&self) -> String {
-        String::from("A simple counter")
-    }
-
-    
-    fn view(&mut self) -> Element<Message> {
-        Column::new()
-            .push(
-                Button::new(&mut self.increment_button, Text::new("Increment"))
-                    .on_press(Message::IncrementPressed),
-            )
-            .push(
-                Text::new(self.value.to_string()).size(50),
-            )
-            .push(
-                Button::new(&mut self.decrement_button, Text::new("Decrement"))
-                    .on_press(Message::DecrementPressed),
-            )
-            .into()
-    }
-
-    
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 1;
-            }
-            Message::DecrementPressed => {
-                self.value = if self.value > 0 { self.value - 1} else { self.value };
-            }
-        }
-
-        Command::none()
-    }
-}
 
 
 fn main() {
-    Counter::run(Settings::default());
     let nat = replicant::Nat { value: 3 };
+
+
+
+
     println!("{}", replicant::Nat::NAME);
 }
--- crdts/src/replicant.rs
+++ crdts/src/replicant.rs
@@ -1,11 +1,11 @@
 pub trait Semilattice: Clone {
   const NAME: &'static str;
+  fn default() -> Self;
   fn join(left: Self, right: Self) -> Self;
 }
 
 
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Nat {
   pub value: u32,
 }
@@ -13,9 +13,31 @@ pub struct Nat {
 
 impl Semilattice for Nat {
   const NAME: &'static str = "Nat";
+  fn default() -> Self {
+    return Nat { value: 0 };
+  }
   fn join(left: Self, right: Self) -> Self {
-      Nat {
-          value: std::cmp::max(left.value, right.value)
-      }
+    Nat {
+        value: std::cmp::max(left.value, right.value)
+    }
+  }
+}
+
+impl Nat {
+  fn increment(&self, v: u32) -> Self {
+    Nat {value: self.value + v}
+  } 
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::Semilattice;
+  quickcheck! {
+    fn commutative(v: u32) -> bool {
+      let initial: super::Nat = Semilattice::default();
+      let incremented = initial.increment(v);
+      Semilattice::join(initial, incremented) == Semilattice::join(incremented, initial) 
+    }
   }
 }
